3f8615e6d283fe993b5ed83b6c5ed5f35c9b5968
rajat <rajatbakshi933@gmail.com>
2025-05-07 00:45:25 +0530

    added favicon

--- Reelify backend/index.js
+++ Reelify backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cors());
+app.use(cors({ origin: "https://reelify-ai-1.onrender.com"}));
 const PORT = process.env.PORT || 5000;
 
 app.use("/",ImageToVideo);
