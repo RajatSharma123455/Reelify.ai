b6a071565c26930e7c21d6be61db3703a7ca5036
rajat <rajatbakshi933@gmail.com>
2025-05-07 21:37:10 +0530

    deployed backend service

--- Reelify backend/index.js
+++ Reelify backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cors({origin:"https://reelify-ai-1.onrender.com/"}));
+app.use(cors({origin:"https://reelify-ai-1.onrender.com"}));
 const PORT = process.env.PORT || 5000;
 
 export const connectDB = async () => {
